b4cc99474d820d07a5e5d76df16d43029e80b818
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-02 18:22:36 +0200

    add models to admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player, Training, Attendance
+
+admin.site.register(Player)
+admin.site.register(Training)
+admin.site.register(Attendance)
+
