5f55f128fc24c6ea0f3469b479fea3a0286270ee
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-02 18:14:17 +0200

    add basic models

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Training(models.Model):
+    date = models.DateTimeField('date')
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+
+class Attendance(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    training = models.ForeignKey(Training, on_delete=models.CASCADE)
+
