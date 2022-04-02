026c5694387bfb52076383397319cd7eee9ab427
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-02 18:48:25 +0200

    add simple interactive route

--- attendance/views.py
+++ attendance/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def player(request, player_name):
+    return HttpResponse(f"You're looking at player {player_name}.")
+
