119c9bf9855d77c156ac824fff231ae79166ae5f
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-02 19:33:13 +0200

    add simple views and submit form

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:player_name>/', views.player, name='player'),
+    path('players/', views.players, name='players'),
+    path('players/<int:player_id>/', views.player, name='player'),
+    path('trainings/', views.trainings, name='trainings'),
+    path('training/<int:training_id>/', views.training, name='training'),
+    path('training/<int:training_id>/submit/', views.submit, name='submit'),
 ]
 
--- attendance/views.py
+++ attendance/views.py
@@ -1,8 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Player, Training
 
-def player(request, player_name):
-    return HttpResponse(f"You're looking at player {player_name}.")
+def players(request):
+    players = Player.objects.all()
+    return render(request, 'players.html', {'players': players})
+
+def player(request, player_id):
+    player = get_object_or_404(Player, pk=player_id)
+    return render(request, 'player.html', {'player': player})
+
+def trainings(request):
+    trainings = Training.objects.all()
+    return render(request, 'trainings.html', {'trainings': trainings})
+
+def training(request, training_id):
+    training = get_object_or_404(Training, pk=training_id)
+    return render(request, 'training.html', {'training': training})
+
+def submit(request, training_id):
+    training = get_object_or_404(Training, pk=training_id)
+
+    choice = request.POST['choice']
+    return HttpResponse(f"submitted choice: {choice}")
+
+    #return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
