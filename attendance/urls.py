401394ec388ec8f59b0344a861c46110bb4e08e7
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-02 19:47:08 +0200

    use generic views

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('players/', views.players, name='players'),
+    path('', views.index, name='index'),
+    path('players/', views.PlayerView.as_view(), name='players'),
     path('players/<int:player_id>/', views.player, name='player'),
-    path('trainings/', views.trainings, name='trainings'),
+    path('trainings/', views.TrainingView.as_view(), name='trainings'),
     path('training/<int:training_id>/', views.training, name='training'),
     path('training/<int:training_id>/submit/', views.submit, name='submit'),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,19 +1,22 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 
+from django.views import generic
+
 from .models import Player, Training
 
-def players(request):
-    players = Player.objects.all()
-    return render(request, 'players.html', {'players': players})
+def index(request):
+    return render(request, 'index.html')
+
+class PlayerView(generic.ListView):
+    model = Player
 
 def player(request, player_id):
     player = get_object_or_404(Player, pk=player_id)
     return render(request, 'player.html', {'player': player})
 
-def trainings(request):
-    trainings = Training.objects.all()
-    return render(request, 'trainings.html', {'trainings': trainings})
+class TrainingView(generic.ListView):
+    model = Training
 
 def training(request, training_id):
     training = get_object_or_404(Training, pk=training_id)
