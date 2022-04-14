bd4553ab27f400f3c858eac1aded50a337fe72a9
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-15 00:16:57 +0200

    add __str__ methods to models

--- attendance/models.py
+++ attendance/models.py
@@ -4,11 +4,20 @@ from django.conf import settings
 class Training(models.Model):
     date = models.DateTimeField('date')
 
+    def __str__(self):
+        return str(self.date.date())
+
 class Player(models.Model):
     name = models.CharField(max_length=50)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
 
 class Attendance(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     training = models.ForeignKey(Training, on_delete=models.CASCADE)
     presence = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.training.date.date()}, {self.player}, {self.presence}"
