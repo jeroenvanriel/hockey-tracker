35964264df561b51f42eae176784c9c9e4550c89
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-14 20:17:05 +0200

    add simple mechanism to store presence

--- attendance/models.py
+++ attendance/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 class Training(models.Model):
     date = models.DateTimeField('date')
 
 class Player(models.Model):
     name = models.CharField(max_length=50)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
 
 class Attendance(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     training = models.ForeignKey(Training, on_delete=models.CASCADE)
-
+    presence = models.BooleanField(default=False)
--- attendance/views.py
+++ attendance/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.urls import reverse
 
-from .models import Player, Training
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+from .models import Player, Training, Attendance
 
 def index(request):
     return render(request, 'index.html')
@@ -14,12 +18,40 @@ class PlayerDetailView(generic.DetailView):
 
 class TrainingListView(generic.ListView):
     model = Training
-class TrainingDetailView(generic.DetailView):
+class TrainingDetailView(LoginRequiredMixin, generic.DetailView):
     model = Training
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['presence'] = True
+
+        try:
+            player = Player.objects.get(user=self.request.user)
+            attendance = Attendance.objects.get(training=self.object, player=player)
+            context['presence'] = attendance.presence
+        except (Player.DoesNotExist, Attendance.DoesNotExist):
+            pass
+
+        return context
+
+@login_required
 def submit(request, training_id):
     training = get_object_or_404(Training, pk=training_id)
 
-    choice = request.POST['choice']
-    return HttpResponse(f"submitted choice: {choice}")
+    try:
+        player = Player.objects.get(user=request.user)
+    except Player.DoesNotExist:
+        return render(request, 'attendance/training_detail.html', {
+            'training': training,
+            'error_message': "No player attached to this account."
+        })
 
+    choice = request.POST['choice'] == 'yes'
+
+    attendance, created = Attendance.objects.update_or_create(
+        player=player,
+        training=training,
+        defaults={'presence': choice}
+    )
+
+    return HttpResponseRedirect(reverse('training', args=(training.id,)))
