64e779806efcd1332628bfb283c92bd6d489fd0a
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-14 17:40:16 +0200

    rename routes to singular names

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('players/', views.PlayerView.as_view(), name='players'),
-    path('players/<int:player_id>/', views.player, name='player'),
-    path('trainings/', views.TrainingView.as_view(), name='trainings'),
+    path('player', views.PlayerView.as_view(), name='players'),
+    path('player/<int:player_id>/', views.player, name='player'),
+    path('training/', views.TrainingView.as_view(), name='trainings'),
     path('training/<int:training_id>/', views.training, name='training'),
     path('training/<int:training_id>/submit/', views.submit, name='submit'),
 ]
