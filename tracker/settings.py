c92924c610bbb135cc827fe9353384e8cac67e21
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-05-08 08:40:15 +0200

    some basic styling using tailwindcss

--- tracker/settings.py
+++ tracker/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 
 # Default primary key field type
