ea07f4debe5a5087318159ca8b46cf3639e8cd81
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-02-26 15:28:26 +0100

    add Fine model to admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Player, Training, Attendance
+from .models import Player, Training, Attendance, Fine
 
 admin.site.register(Player)
 admin.site.register(Training)
 admin.site.register(Attendance)
-
+admin.site.register(Fine)
